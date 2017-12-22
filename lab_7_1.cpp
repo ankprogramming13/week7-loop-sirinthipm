d7dcde9760673673496a672d63cb30a634a86ab4
sirinthipm <33145165+sirinthipm@users.noreply.github.com>
2017-12-22 13:28:09 +0700

    แก้

--- lab_7_1.cpp
+++ lab_7_1.cpp
@@ -10,18 +10,9 @@ int main() {
 
   // ศึกษาตัวอย่างการใช้ for
   for(i = 0; i < 10 ; i++) {
-    cout << "Round " << i << endl;
+    cout << "Hello World" << i << endl;
   }
 
-  // TODO : แก้ไขส่วนนี้ด้วยการใช้ for
-  cout << "Hello World 1" << endl;
-  cout << "Hello World 2" << endl;
-  cout << "Hello World 3" << endl;
-  cout << "Hello World 4" << endl;
-  cout << "Hello World 5" << endl;
-  cout << "Hello World 6" << endl;
-  cout << "Hello World 7" << endl;
-  cout << "Hello World 8" << endl;
 
   return 0;
 }

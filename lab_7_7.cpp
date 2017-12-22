87979051b07fc7c6fb30c00a12b15acfb67e0f13
sirinthipm <33145165+sirinthipm@users.noreply.github.com>
2017-12-22 14:11:49 +0700

    แก้

--- lab_7_7.cpp
+++ lab_7_7.cpp
@@ -2,7 +2,24 @@
 
 using namespace std;
 
-int main () {
+int main ()
+{
+
+
+    int a = 0;
+    int num, sum=0, avg;
+
+    // while loop execution
+    while( a < 5 )
+    {
+        cout << "Enter" << a + 1 << "Number: ";
+        cin >> num;
+        sum = sum + num;
+        a++;
+    }
+    avg = (float) sum / 5;
+    cout << "Total" << "\t" << avg;
+  
 
   // เขียนโปรแกรมเพื่อรับค่าตัวเลข 5 จำนวน และหาค่าเฉลี่ย
 

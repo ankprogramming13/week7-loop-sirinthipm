f8e41dd0d2593e9f1f81c738ecc917fded2562c5
sirinthipm <33145165+sirinthipm@users.noreply.github.com>
2017-12-22 14:05:42 +0700

    แก้

--- lab_7_4.cpp
+++ lab_7_4.cpp
@@ -40,7 +40,26 @@ int main () {
   cout << "*" << endl;
 
   // เขียนโปรแกรมให้ทำงานได้ตามข้างต้นโดยใช้ Loop
+ // Local variable declaration:
+    int a = 10;
 
+    // while loop execution
+    while( a < 3 ) {
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "***" << endl;
+  cout << "****" << endl;
+  cout << "***" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+  cout << "**" << endl;
+  cout << "*" << endl;
+      a++;
+    }
+
+  // Without loop
 
   return 0;
 }
